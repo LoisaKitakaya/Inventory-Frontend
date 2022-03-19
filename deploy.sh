26ba815e615133458dc38680a45956f33f57cb41
Freedom Loisa <kitakayaloisa@gmail.com>
2022-03-19 14:19:42 +0300

    deployment settings

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:LoisaKitakaya/Inventory-Frontend.git main
+git push -f git@github.com:LoisaKitakaya/Inventory-Frontend.git main:gh-pages
 
 cd -
